37b109ca0c916264c945b8f3ca5d24d330acd842
Alexis Villegas Torres <alexisvt@gmail.com>
2017-09-26 15:38:33 -0600

    adding pipes sample

--- my-app/src/app/courses/courses.component.ts
+++ my-app/src/app/courses/courses.component.ts
@@ -12,6 +12,13 @@ export class CoursesComponent implements OnInit {
   courses: string[];
   isActive: boolean;
   emailText: string;
+  objPipeSample = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1),
+  };
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
     this.isActive = true;
