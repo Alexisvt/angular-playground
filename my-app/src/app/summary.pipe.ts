a88eeb3406ee33ead7e22e84613732eb066b02ec
Alexis Villegas Torres <alexisvt@gmail.com>
2017-09-26 16:00:41 -0600

    continue the custom pipe sample

--- my-app/src/app/summary.pipe.ts
+++ my-app/src/app/summary.pipe.ts
@@ -4,11 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'summary',
 })
 export class SummaryPipe implements PipeTransform {
-  transform(value: string, args?: any): any {
+  transform(value: string, limit?: any, anotherParam?: any): any {
     if (!value) {
       return null;
     }
+    const actualLimit = limit ? limit : 50;
 
-    return value.substr(0, 40) + '...';
+    return value.substr(0, actualLimit) + '...';
   }
 }
