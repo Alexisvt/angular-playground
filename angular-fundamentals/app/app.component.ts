29a9d490fb75d504628780fee869410f4462efa9
Alexis Villegas Torres <alexisvt@outlook.com>
2017-11-26 20:25:12 -0600

    adding more samples

--- angular-fundamentals/app/app.component.ts
+++ angular-fundamentals/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Event } from '@angular/platform-browser/src/facade/browser';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,14 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
   <div class="app">
-    {{title}}
+    <h1 [innerHTML]="title" ></h1>
+    <h1> {{title}} </h1>
+    <img [src]="logo" />
+    <button (click)="handleClick()"> Change name </button>
+    <input 
+    type="text" 
+    [ngModel]="name"
+    (ngModelChange)="handleChange($event)"
+     />
+     <input 
+     type="text" 
+     [(ngModel)]="name"
+      />
+    <div> {{name}} </div>
+    <div>
+      <h2>Working with #ref variables sample</h2>
+      <div>
+        <button (click)="handleClickRef(lastName.value)">Get value</button>
+        <input type="text" #lastName>
+        <p>{{lastNm}}</p>
+      </div>
+    </div>
   </div>
   `,
 })
 export class AppComponent {
   title: string;
+  logo = 'http://lorempixel.com/50/50/';
+  name = 'Alexis';
+  lastNm: string;
 
   constructor() {
     this.title = 'Ultimate Angular';
+  }
+
+  handleClick() {
+    this.name = 'Salomon';
+  }
+
+  handleClickRef(value: string) {
+    this.lastNm = value;
+  }
+
+  handleBlur(event: any) {
+    this.name = event.target.value;
+  }
+
+  handleChange(value: string) {
+    this.name = value;
+  }
+
+  handleInput(event: any) {
+    this.name = event.target.value;
   }
 }
--- angular-fundamentals/app/app.module.ts
+++ angular-fundamentals/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,7 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, CommonModule],
+  imports: [BrowserModule, CommonModule, FormsModule],
   bootstrap: [AppComponent],
   declarations: [AppComponent],
 })
